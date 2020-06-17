a38fb5005000b16c4a49ae537388bab3febe57d2
vuxuanhung1999mta@gmail.com <vuxuanhung1999mta@gmail.com>
2020-06-17 08:26:03 +0700

    Hưng: update Index SanPham

---
+++ WebBanHang/Controllers/SanPhamController.cs
@@ -0,0 +1,100 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models.Entity;
+
+namespace WEBBANHANG.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        BanHangEntity db;
+        List<SanPham> getlstSanpham()
+        {
+            db = new BanHangEntity();
+            var lst = db.SanPhams.SqlQuery("Select * from SanPham").ToList<SanPham>();
+            return lst;
+        }
+        // GET: SanPham
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? loaiSanPhamID, int? khoangGiaTu, int? khoangGiaDen,
+            int? page)
+        {
+            db = new BanHangEntity();
+            /*if (Session["username"] == null)
+                return RedirectToAction("/Index", "Users");
+            else
+            {*/
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "name";
+            ViewBag.GiaBanSortParm = sortOrder == "giaban" ? "giaban_desc" : "giaban";
+            ViewBag.KyHieuSortParm = sortOrder == "kyhieu" ? "kyhieu_desc" : "kyhieu";
+            ViewBag.loaiSanPhamID = new SelectList(db.LoaiSanPhams, "LoaiSanPhamID", "TenLoai", loaiSanPhamID);
+            ViewBag.khoangGiaTu = khoangGiaTu;
+            ViewBag.khoangGiaDen = khoangGiaDen;
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            string query = "Select * from SanPham ";
+            string qrten = "";
+            string qrgiatu = "";
+            string qrgiaden = "";
+            if (!String.IsNullOrEmpty(searchString))
+                qrten = "(TenSanPham like N'%" + searchString + "%' or KyHieuSanPham like N'%" + searchString + "%')";
+
+            if (khoangGiaTu.HasValue)
+                qrgiatu = "GiaBan >= " + khoangGiaTu;
+
+            if (khoangGiaDen.HasValue)
+                qrgiaden = "GiaBan <= " + khoangGiaDen;
+
+            if (loaiSanPhamID.HasValue)
+                query = query + ", LoaiSanPham where LoaiSanPham.LoaiSanPhamID = SanPham.LoaiSanPhamID and SanPham.LoaiSanPhamID = " + loaiSanPhamID;
+            if (!String.IsNullOrEmpty(searchString) || (khoangGiaTu.HasValue) || (khoangGiaDen.HasValue))
+                query = query + (loaiSanPhamID.HasValue ? " and " : " where ")
+                    + qrten
+                    + (qrgiatu != "" ? (qrten != "" ? " and " : "") + qrgiatu : "")
+                    + (qrgiaden != "" ? (qrgiatu != "" ? " and " : "") + qrgiaden : "");
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    query = query + " order by TenSanPham desc ";
+                    break;
+                case "name":
+                    query = query + " order by TenSanPham ";
+                    break;
+                case "kyhieu":
+                    query = query + " order by KyHieuSanPham ";
+                    break;
+                case "kyhieu_desc":
+                    query = query + " order by KyHieuSanPham desc ";
+                    break;
+                case "giaban":
+                    query = query + " order by GiaBan ";
+                    break;
+                case "giaban_desc":
+                    query = query + " order by GiaBan desc ";
+                    break;
+                default:  // new pro
+                    query = query + " order by LaSanPhamMoi desc";
+                    break;
+            }
+
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            var lstLoaiSanPham = db.SanPhams.SqlQuery(query);
+            return View(lstLoaiSanPham.ToPagedList(pageNumber, pageSize));
+            //}
+        }
+    }
+}
