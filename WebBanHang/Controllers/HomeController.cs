a42033c33b3f7ca0a7733aa16e5daf345ec7f049
vuxuanhung1999mta@gmail.com <vuxuanhung1999mta@gmail.com>
2020-06-23 13:28:13 +0700

    Hưng: update Home (Trang chủ & Đơn Đặt Hàng)

--- WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using WebBanHang.Models.Entity;
 using WebBanHang.Models;
+using PagedList;
 
 
 namespace WebBanHang.Controllers
@@ -17,7 +18,111 @@ namespace WebBanHang.Controllers
     }
     public class HomeController : Controller
     {
+        BanHangEntity db = new BanHangEntity();
         public ActionResult Index()
+        {
+            if (Session["username"] == null)
+                return RedirectToAction("/Index", "Users");
+            else
+            {
+                int id = (int)Session["usernameid"];
+                var donDatHang = db.DonDatHangs.Where(x => x.TaiKhoanDatHangID == id && x.TinhTrang == 0).FirstOrDefault();
+
+                return View();
+            }
+        }
+        //Home
+        public ActionResult ListProductView(int? loaiSanPhamID, int? khoangGiaTu, int? khoangGiaDen, string laMoi, string sortOrder, string currentFilter, string tenSanPham, int? page)
+        {
+            if (Session["username"] == null)
+                return RedirectToAction("/Index", "Users");
+            else
+            {
+                ViewBag.CurrentSort = sortOrder;
+                ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+                ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+                ViewBag.khoangGiaTu = khoangGiaTu;
+                ViewBag.khoangGiaDen = khoangGiaDen;
+                if (laMoi != null)
+                {
+                    ViewBag.laMoi = true;
+
+                }
+                if (tenSanPham != null)
+                {
+                    page = 1;
+                }
+                else
+                {
+                    tenSanPham = currentFilter;
+                }
+
+                ViewBag.CurrentFilter = tenSanPham;
+                if (loaiSanPhamID.HasValue)
+                {
+                    //lstSP = db.SanPhams.Where(x => x.LoaiSanPhamID == loaiSanPhamID).ToList();
+                    string query = "Select * from SanPham where LoaiSanPhamID = " + loaiSanPhamID;
+                    string qrten = "";
+                    string qrgiatu = "";
+                    string qrgiaden = "";
+                    string qrlaMoi = "";
+                    if (!String.IsNullOrEmpty(tenSanPham))
+                        qrten = " and TenSanPham like N'%" + tenSanPham + "%'";
+
+                    if (khoangGiaTu.HasValue)
+                        qrgiatu = " and GiaBan >= " + khoangGiaTu;
+
+                    if (khoangGiaDen.HasValue)
+                        qrgiaden = " and GiaBan <= " + khoangGiaDen;
+                    if (!String.IsNullOrEmpty(laMoi))
+                    {
+                        if (laMoi == "on")
+                        {
+                            qrlaMoi = " and LaSanPhamMoi =" + 0;
+                        }
+                        if (laMoi != "")
+                        {
+                            qrlaMoi = " and LaSanPhamMoi =" + 1;
+                        }
+                    }
+                    if (!String.IsNullOrEmpty(tenSanPham) || (khoangGiaTu.HasValue) || (khoangGiaDen.HasValue) || (!String.IsNullOrEmpty(laMoi)))
+                        query = query + " " + qrten + (qrgiatu != "" ? (qrten != "" ? "  " : "") + qrgiatu : "") + (qrgiaden != "" ? (qrgiatu != "" ? "  " : "") + qrgiaden : "") + (qrlaMoi != "" ? (qrgiaden != "" ? "  " : "") + qrlaMoi : "");
+
+                    int pageSize = 4;
+                    int pageNumber = (page ?? 1);
+                    //ViewBag.lstSP = lstSP;
+                    ViewBag.loaiSanPhamID = loaiSanPhamID;
+                    var listSP = db.SanPhams.SqlQuery(query);
+                    return View(listSP.ToPagedList(pageNumber, pageSize));
+                }
+                else
+                {
+                    return View();
+                }
+
+
+            }
+        }
+        [HttpPost]
+        public ActionResult ListProduct(int id)
+        {
+            return View();
+        }
+
+        public ActionResult DetailProductView(int sanPhamID)
+        {
+            if (Session["username"] == null)
+                return RedirectToAction("/Index", "Users");
+            else
+            {
+                var sanPham = db.SanPhams.Include(x => x.NhaCungCap).Include(x => x.ThuocTinhSanPhams.Select(y => y.ThuocTinh)).Where(x => x.SanPhamID == sanPhamID).FirstOrDefault();
+                ViewBag.sanPham = sanPham;
+                return View(sanPham);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult DetailProduct(int id)
         {
             return View();
         }
@@ -35,18 +140,53 @@ namespace WebBanHang.Controllers
 
             return View();
         }
+        // Đơn đặt hàng
         public ActionResult ListDonDat()
         {
-            return View();
+            using (var db = new BanHangEntity())
+            {
+                if (Session["username"] == null)
+                {
+                    return RedirectToAction("/Index", "Users");
+                }
+                else
+                {
+                    int id = (int)Session["usernameid"];
+                    var donDatHang = db.DonDatHangs.Include(x => x.ChiTietDonDatHangs.Select(y => y.SanPham)).Where(x => x.TaiKhoanDatHangID == id).ToList();
+                    ViewBag.donDatHang = donDatHang;
+                    return View();
+                }
+            }
+        }
+        public ActionResult ChiTietDon(int donDatHangID)
+        {
+            using (var db = new BanHangEntity())
+            {
+                if (Session["username"] == null)
+                {
+                    return RedirectToAction("/Index", "Users");
+                }
+                else
+                {
+                    var donDatHang = db.DonDatHangs.Include(y => y.ChiTietDonDatHangs.Select(z => z.SanPham)).Where(x => x.DonDatHangID == donDatHangID).FirstOrDefault();
+                    //ListSanPham lstSP = new ListSanPham();
+                    //foreach(var i in donDatHang.ChiTietDonDatHangs)
+                    //{
+                    //    SanPham sp = i.SanPham;
+                    //    lstSP.SanPhams.Add(sp);
+                    //    lstSP.SoLuong = i.SoLuong;
+
+                    //}
+                    ViewBag.donDatHang = donDatHang;
+                    return View();
+                }
+            }
         }
         public ActionResult CartView()
         {
             return View();
         }
-        public ActionResult ChiTietDon()
-        {
-            return View();
-        }
+        
         public ActionResult DeleteItem()
         {
             return View();
@@ -62,25 +202,7 @@ namespace WebBanHang.Controllers
         {
             return View();
         }
-        public ActionResult ListProductView()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult ListProduct()
-        {
-            return View();
-        }
-        public ActionResult DetailProductView()
-        {
-            return View();
-
-        }
-        [HttpPost]
-        public ActionResult DetailProduct()
-        {
-            return View();
-        }
+        
         public ActionResult Create()
         {
 
